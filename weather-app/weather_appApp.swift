f485efeb64f4c3c416d33c8564ab52eebed30704
esaibev <esaias.bevegard@gmail.com>
2023-11-20 12:23:43 +0100

    Initial Commit

---
+++ weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Esaias Bevegård on 2023-11-20.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
